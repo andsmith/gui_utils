ff517eb5855f51cf8325e0138284078bc7bc2eae
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2022-02-19 13:39:05 -0800

    debugged gui picker

--- gui_picker.py
+++ gui_picker.py
@@ -2,19 +2,20 @@ import tkinter as tk
 import logging
 
 
-def choose_item_dialog(prompt, labels, values, button_text="Continue...",
-                       label_params=None, item_params=None):
+def choose_item_dialog(labels, prompt="Please select one\nof the following:",
+                       button_text="Continue...", label_params=None, item_params=None):
     """
     Open a dialog box with some text, radio buttons, and continue button.
-    :param prompt:  text at top
     :param labels:  text for radio buttons
-    :param values:  values for radio buttons
+    :param prompt:  Label above items
     :param button_text:  text for selection button
     :param label_params:  dict with kwargs to tk.Label
     :param item_params:  dict with kwargs for tk.RadioButton
-    :return:
+    :return: index of label item clicked or None for window closed
     """
     indent_px = 20
+
+    # update defaults
     lp = {'padx': indent_px, 'pady': 5}  # defaults
     if label_params is not None:
         lp.update(label_params)
@@ -26,29 +27,32 @@ def choose_item_dialog(prompt, labels, values, button_text="Continue...",
     item_params = ip
 
     root = tk.Tk()
+    selection = tk.IntVar(value=0)
 
-    selection = tk.IntVar(value=1)
-    selection.set(1)  # i+1
+    # Default is first item
+    selection.set(0)
     final_choice = [0]
+    finished=[False]
 
     def show_choice():
-        n = selection.get() - 1
-        logging.info("Item %i selected (text='%s', value=%i)" % (n, labels[n], values[n]))
+        n = selection.get()
+        logging.info("Item %i selected (%s)" % (n, labels[n]))
         final_choice[0] = n
 
     def finish():
+        finished[0]=True
         logging.info("Selection finalized:  %s" % (final_choice,))
         root.destroy()
 
     label = tk.Label(root, text=prompt, **label_params)
     label.pack(pady=5)
 
-    for i, (label, value) in enumerate(zip(labels, values)):
+    for i, label in enumerate(labels):
         rb = tk.Radiobutton(root,
                             text=label,
                             variable=selection,
                             command=show_choice,
-                            value=value,
+                            value=i,
                             **item_params)
         rb.pack(anchor=tk.W)
 
@@ -59,16 +63,15 @@ def choose_item_dialog(prompt, labels, values, button_text="Continue...",
     button.pack(pady=20)
 
     root.mainloop()
+    if not finished[0]:
+        return None
     return final_choice[0]
 
 
 def _test_picker():  # requires user interaction
-    for _ in range(3):
-        choice = choose_item_dialog("Please select one\nof the following:",
-                                    label_params=dict(justify=tk.LEFT),
-                                    labels=['item 1', 'item 2 is longer', 'item 3'],
-                                    values=[1, 2, 3])
-        print("User chose %i." % (choice,))
+    choice = choose_item_dialog(label_params=dict(justify=tk.LEFT),
+                                labels=['item 1', 'item 2 is longer', 'item 3'])
+    print("User chose:  %s" % (choice,))
 
 
 if __name__ == "__main__":
